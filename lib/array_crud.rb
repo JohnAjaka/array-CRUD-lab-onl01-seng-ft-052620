f65998aeaa6cebf3e1b72a29e29c21853f448a47
John Ajaka <johnajaka96@gmail.com>
2020-05-27 16:57:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [lab, newfie, retriever, bulldog]
+  dog_breeds = [lab, newfie, retriever, bulldog]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  dog_breeds << frenchie
 end
 
 def add_element_to_start_of_array(array, element)
