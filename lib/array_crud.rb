7d8ef2e2a0c3590682dbfad919fdb4f6403c078c
John Ajaka <johnajaka96@gmail.com>
2020-05-27 16:59:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   dog_breeds << frenchie
-  puts dog_breeds
+  puts dog_breeds.inspect
 end
 
 def add_element_to_start_of_array(array, element)
