bda5e4c937f2f19a5b33f962e43525c451f5e28d
John Ajaka <johnajaka96@gmail.com>
2020-05-27 17:04:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  dog_breeds = ["lab", "newfie", "retriever", "bulldog"]
-  dog_breeds << "frenchie"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
